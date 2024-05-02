38670dad87a3c1e2cb236d24e5d32be3b9dcd073
MaxPredathor <max.gilli98@gmail.com>
2024-05-02 11:03:31 +0200

    MILESTONE 2

--- csharp-gestore-eventi/Program.cs
+++ csharp-gestore-eventi/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace csharp_gestore_eventi
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace csharp_gestore_eventi
 {
     internal class Program
     {
@@ -6,20 +8,49 @@
         {
             try
             {
-                Evento test = new Evento("Concerto", new DateTime(2024, 6, 15), 100);
+                Console.WriteLine("Inserisci il nome dell'evento:");
+                string nomeEvento = Console.ReadLine();
 
-                test.PrenotaPosti(50);
-                test.DisdiciPosti(20);
-                Console.WriteLine(test.ToString());
+                Console.WriteLine("Inserisci la data dell'evento (gg/mm/yyyy):");
+                DateTime dataEvento = Convert.ToDateTime(Console.ReadLine());
+
+                Console.WriteLine("Inserisci il numero di posti totali:");
+                int numeroPostiEvento = Convert.ToInt32(Console.ReadLine());
+
+                Evento ufficiale = new Evento(nomeEvento, dataEvento, numeroPostiEvento);
+
+                Console.WriteLine("Quanti posti desideri prenotare?");
+                int numeroPrenotazioni = Convert.ToInt32(Console.ReadLine());
+
+                ufficiale.PrenotaPosti(numeroPrenotazioni);
+                Console.WriteLine($"Numero posti prenotati = {ufficiale.PostiPrenotati}");
+                Console.WriteLine($"Numero posti disponibili = {ufficiale.CapienzaMassima - ufficiale.PostiPrenotati}");
+
+                bool a = true;
+                while (a || ufficiale.PostiPrenotati == 0)
+                {
+                    Console.WriteLine("Vuoi disdire dei posti (si/no)?");
+                    string risposta = Console.ReadLine();
+                    if (risposta == "si")
+                    {
+                        Console.WriteLine("Indica il numero di posti da disdire:");
+                        int postiDaDisdire = Convert.ToInt32(Console.ReadLine());
+                        ufficiale.DisdiciPosti(postiDaDisdire);
+                        Console.WriteLine($"Numero posti prenotati = {ufficiale.PostiPrenotati}");
+                        Console.WriteLine($"Numero posti disponibili = {ufficiale.CapienzaMassima - ufficiale.PostiPrenotati}");
+                    }
+                    else if (risposta == "no")
+                        a = false;
+                    else
+                        break;
+                        
+                }
+                
             }
             catch (ArgumentException e)
             {
                 Console.WriteLine($"Errore: {e.Message}");
             }
-            //catch (InvalidOperationException e)
-            //{
-            //    Console.WriteLine($"Operazione non valida: {e.Message}");
-            //}
             catch (Exception e)
             {
                 Console.WriteLine($"Si è verificato un errore: {e.Message}");
